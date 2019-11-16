7e5f1bcbce56de60435c5525c30e8fe8ff6ac752
Nguyen Thi Thu Huong <huong.nguyen@pn.com.vn>
2019-11-16 11:02:54 +0700

    recyclerview

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvNumber;
+    Button btnAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvNumber = findViewById(R.id.recycle_Number);
+        rvNumber.setLayoutManager(new LinearLayoutManager(this));
+
+        final List<String> data1 = new ArrayList<>();
+        for (int i = 0; i < 5 ;i++){
+            data1.add("Huong");
+            data1.add("Amy");
+            data1.add("Nguyen");
+        }
+        final NumberAdapter adapter = new NumberAdapter();
+        adapter.data = data1;
+        rvNumber.setAdapter(adapter);
+
+        findViewById(R.id.addNew).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data1.add("new new");
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
